136816077bf190f1daddf8e9573bd5bc2d3b9b98
allysonhalley <allysonhalley@gmail.com>
2025-04-16 20:44:04 -0300

    first commit

---
+++ src/main/java/com/hefti/kd1kturboapi/Kd1kTurboApiApplication.java
@@ -0,0 +1,13 @@
+package com.hefti.kd1kturboapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kd1kTurboApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kd1kTurboApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hefti/kd1kturboapi/Kd1kTurboApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hefti.kd1kturboapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kd1kTurboApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
